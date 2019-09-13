7eb6173b7aea2119ba37685b6f8ccf43acc60df8
Shreyam Shah <shreyam.shah13@gmail.com>
2019-09-13 22:40:53 +0530

    heap sort
    Signed-off-by: Shreyam Shah <shreyam.shah13@gmail.com>

--- README.md
+++
@@ -1,9 +0,0 @@
-# Sort Tests
-Analyse sort times for insertion,quick and merge sort
-
-# How to run this file
-This takes a command line integer which denotes which sort is to be performed.
-
-0. Insertion Sort
-1. Merge Sort
-2. Quick Sort
--- sortTest.java
+++ sortTest.java
@@ -240,4 +240,55 @@ public class sortTest {
 			}
 		}
 	}
+
+	public void sortH(ArrayList<Integer> values) {
+		int n = values.size();
+
+		// Build heap (rearrange array)
+		for (int i = n / 2 - 1; i >= 0; i--)
+			heapify(values, n, i);
+
+		// One by one extract an element from heap
+		for (int i = n - 1; i >= 0; i--) {
+			// Move current root to end
+				int num1 = values.get(i);
+				int num2 = values.get(0);
+				values.set(i, num2);
+				values.set(0, num1);
+
+			//int temp = arr[0];
+			//arr[0] = arr[i];
+			//arr[i] = temp;
+
+			// call max heapify on the reduced heap
+			heapify(values, i, 0);
+		}
+	}
+
+	// To heapify a subtree rooted with node i which is
+	// an index in arr[]. n is size of heap
+	void heapify(ArrayList<Integer> values, int n, int i) {
+		int largest = i; // Initialize largest as root
+		int l = 2 * i + 1; // left = 2*i + 1
+		int r = 2 * i + 2; // right = 2*i + 2
+
+		// If left child is larger than root
+		if (l < n && values.get(l) > values.get(largest))
+			largest = l;
+
+		// If right child is larger than largest so far
+		if (r < n && values.get(r) > values.get(largest))
+			largest = r;
+
+		// If largest is not root
+		if (largest != i) {
+			int swap1 = values.remove(i);
+			int swap2 = values.remove(largest);
+			values.set(i, swap2);
+			values.set(largest, swap1);
+
+			// Recursively heapify the affected sub-tree
+			heapify(values, n, largest);
+		}
+	}
 }
